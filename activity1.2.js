e26f2a11ff22699f07f91b1e242c616c97ea8dc3
Bhanumathi0502 <bbhanuprasad82@gmail.com>
2026-04-29 14:45:28 +0530

    uploaded by Bhanumathi

---
+++ activity1.2.js
@@ -0,0 +1,15 @@
+var terrestrials = ['lion','fox','elephant','zebra'];
+var aquatics = ['blue whael','shark','Octopus'];
+
+var allAnimals=[...terrestrials,...aquatics];
+console.log(allAnimals);
+
+
+var a=5;
+var b=6;
+
+var c=a+b;
+
+console.log(a);
+console.log(b);
+console.log(c);
